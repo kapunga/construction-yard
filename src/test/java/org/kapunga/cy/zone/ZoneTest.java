98481277c0c70406822ff09e1537a6d417bd7974
Paul J Thordarson <kapunga@gmail.com>
2013-05-26 14:32:27 -0400

    Testing and Code Coverage added
    Added Cobertura and Junit for testing.

---
+++ src/test/java/org/kapunga/cy/zone/ZoneTest.java
@@ -0,0 +1,14 @@
+package org.kapunga.cy.zone;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ZoneTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
